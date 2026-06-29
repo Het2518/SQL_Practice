80428b72a9f7baa9defce7c78aaaa17f86e26c59
het2518 <hetmonpara2022@gmail.com>
2026-06-30 02:14:01 +0530

    chore: remove unused TypeScript configuration files and maintenance utility scripts

--- truncate.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const lines = fs.readFileSync('src/App.jsx', 'utf8').split('\n');
-fs.writeFileSync('src/App.jsx', lines.slice(0, 477).join('\n'), 'utf8');
-console.log('Done. Lines now:', lines.slice(0,477).length);
